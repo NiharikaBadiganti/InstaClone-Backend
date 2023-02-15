c5403e035b1d67cbb44f51587de691698b69e0b2
NiharikaBadiganti <badigantiniharika@gmail.com>
2023-02-15 07:56:44 +0530

    Again Change Latest2

--- src/RoutePostData.js
+++ src/RoutePostData.js
@@ -15,7 +15,7 @@ postRouter.use(bodyparser.json());
 
 var cloudinary = require('cloudinary').v2;
 
-app.use(function(req, res, next) {
+postRouter.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();

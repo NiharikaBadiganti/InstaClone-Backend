fbbe3ee0025e5c4aaa3e774d4ce34eaa2a3b51dd
NiharikaBadiganti <badigantiniharika@gmail.com>
2023-02-15 08:30:13 +0530

    Changes in db connection

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-mongoose.connect(process.env.DATABASE_URI,{ useNewUrlParser: true, useUnifiedTopology: true,})
-    .then(() => console.log("Connected Successfully"))    
-    .catch((error)=>console.log(error))
+const db = 'mongodb+srv://Niharika:password@cluster0.gfgtat0.mongodb.net/?retryWrites=true&w=majority'
+mongoose
+    .connect(db, { 
+        useNewUrlParser: true,
+        useCreateIndex: true
+      })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>
